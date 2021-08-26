eeeed412db73aed9ea02229a8a64420a84788910
Mattias Lightstone <mattias.lightstone@gmail.com>
2021-08-26 10:54:27 -0700

    fixup

--- src/components/Template.tsx
+++ src/components/Template.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link, NavLink, Route, Switch } from 'react-router-dom';
 import Wallet from '../features/wallet/Wallet';
 import apeImage from '../images/ape.png';
-import {Popover, PopoverArrow, PopoverBody, PopoverCloseButton, PopoverContent, PopoverHeader, PopoverTrigger} from '@chakra-ui/react';
+import {Popover, PopoverBody, PopoverCloseButton, PopoverContent, PopoverHeader, PopoverTrigger} from '@chakra-ui/react';
 
 interface Props {
 }
@@ -63,7 +63,7 @@ export const Template = (props: Props) => {
 
 const Header = () => {
     return (
-        <div id="header" className="flex flex-col items-center p-3 justify-between md:flex-row md:items-start">
+        <div id="header" className="flex flex-col items-center p-3 justify-between md:flex-row md:items-center">
             <div id="Logo" className="-m-6">
                 <Link to="/">
                     <img src={apeImage} alt={"APY"} className="h-32 w-32"/>
@@ -82,16 +82,26 @@ const WalletSettings = () => {
     return (
         <Popover>
             <PopoverTrigger>
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                    <path fillRule="evenodd" d="M11.49 3.17c-.38-1.56-2.6-1.56-2.98 0a1.532 1.532 0 01-2.286.948c-1.372-.836-2.942.734-2.106 2.106.54.886.061 2.042-.947 2.287-1.561.379-1.561 2.6 0 2.978a1.532 1.532 0 01.947 2.287c-.836 1.372.734 2.942 2.106 2.106a1.532 1.532 0 012.287.947c.379 1.561 2.6 1.561 2.978 0a1.533 1.533 0 012.287-.947c1.372.836 2.942-.734 2.106-2.106a1.533 1.533 0 01.947-2.287c1.561-.379 1.561-2.6 0-2.978a1.532 1.532 0 01-.947-2.287c.836-1.372-.734-2.942-2.106-2.106a1.532 1.532 0 01-2.287-.947zM10 13a3 3 0 100-6 3 3 0 000 6z" clipRule="evenodd" />
-                </svg>
+                <div className="hover:bg-gray-200 rounded-full cursor-pointer p-1">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                        <path fillRule="evenodd" d="M11.49 3.17c-.38-1.56-2.6-1.56-2.98 0a1.532 1.532 0 01-2.286.948c-1.372-.836-2.942.734-2.106 2.106.54.886.061 2.042-.947 2.287-1.561.379-1.561 2.6 0 2.978a1.532 1.532 0 01.947 2.287c-.836 1.372.734 2.942 2.106 2.106a1.532 1.532 0 012.287.947c.379 1.561 2.6 1.561 2.978 0a1.533 1.533 0 012.287-.947c1.372.836 2.942-.734 2.106-2.106a1.533 1.533 0 01.947-2.287c1.561-.379 1.561-2.6 0-2.978a1.532 1.532 0 01-.947-2.287c.836-1.372-.734-2.942-2.106-2.106a1.532 1.532 0 01-2.287-.947zM10 13a3 3 0 100-6 3 3 0 000 6z" clipRule="evenodd" />
+                    </svg>
+                </div>
             </PopoverTrigger>
             <PopoverContent>
-                {/* <PopoverArrow/> */}
                 <PopoverCloseButton/>
-                <PopoverHeader>Settings</PopoverHeader>
+                <PopoverHeader>Transaction Settings</PopoverHeader>
                 <PopoverBody>
-                    This is where settings go
+                    <text className="Slippage"></text>
+                    <div className="flex flex-row justify-center gap-4 items-center">
+                        <div>0.1%</div>
+                        <div>0.5%</div>
+                        <div>1%</div>
+                        <div className="shadow-inner flex flex-row items-center p-2 w-16 rounded-xl">
+                            <input type="number" placeholder="2" className="min-w-0"></input>
+                            <div>%</div>
+                        </div>
+                    </div>
                 </PopoverBody>
             </PopoverContent>
         </Popover>
