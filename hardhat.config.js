ab4b95fd7889d756106887cebb1fc664166319c9
Mattias Lightstone <mattias.lightstone@gmail.com>
2021-09-09 11:23:47 -0700

    Add dotenv to hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,6 @@
 require("@nomiclabs/hardhat-waffle");
 require('@typechain/hardhat');
+require('dotenv').config();
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
