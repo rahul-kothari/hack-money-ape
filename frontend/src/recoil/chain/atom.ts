15ae633045953f3693aac94450bc78f7e6d52c39
Mattias Lightstone <mattias.lightstone@gmail.com>
2021-10-07 14:00:10 -0700

    Use env variable to set network

--- frontend/src/recoil/chain/atom.ts
+++ frontend/src/recoil/chain/atom.ts
@@ -1,6 +1,14 @@
 import {atom} from 'recoil';
 
+const initial = (() => {
+    if(process.env.REACT_APP_NETWORK){
+        return process.env.REACT_APP_NETWORK
+    } else {
+        return "mainnet"
+    }
+})()
+
 export const chainNameAtom = atom({
     key: 'chainName',
-    default: 'mainnet'
+    default: initial
 })
