c3228472ab439cd305e6d6b20a22834e73122ced
Mattias Lightstone <mattias.lightstone@gmail.com>
2021-10-13 14:35:23 -0700

    Full width card

--- frontend/src/components/Reusable/Card.tsx
+++ frontend/src/components/Reusable/Card.tsx
@@ -7,12 +7,13 @@ interface Props {
 const Card: React.FC<Props> = (props) => {
     return (
         <Flex
-            id="parameters"
+            id="card"
             flexDir="column"
             alignItems="stretch"
             bgColor="indigo.100"
             rounded="2xl"
             shadow="lg"
+            width="full"
             px={4}
             py={2}
         >
