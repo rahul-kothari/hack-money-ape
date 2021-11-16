2cfea0f71e9901e6b267541c32d74b6451e534b2
Mattias Lightstone <mattias.lightstone@gmail.com>
2021-11-15 21:53:17 -0700

    Add a beta disclaimer

--- frontend/src/components/Template.tsx
+++ frontend/src/components/Template.tsx
@@ -1,6 +1,8 @@
 import { Box, Flex } from '@chakra-ui/layout';
 import React from 'react'
 import { Route, Switch } from 'react-router-dom';
+import { useRecoilState } from 'recoil';
+import { disclaimerAtom } from '../recoil/disclaimer/atom';
 import Footer from './Footer';
 import Header from './Header';
 import { Notifications } from './Notifications';
@@ -8,11 +10,14 @@ import { Decollateralize } from './Pages/Decollateralize';
 import Home from './Pages/Home';
 import { Ladder } from './Pages/Ladder';
 import { YTC } from './Pages/YTC';
+import { Alert } from './Reusable/Alert';
 
 interface Props {
 }
 
 export const Template: React.FC<Props> = (props: Props) => {
+    const [open, setOpen] = useRecoilState(disclaimerAtom);
+
     return (
         <Flex
             bg="background"
@@ -20,6 +25,12 @@ export const Template: React.FC<Props> = (props: Props) => {
             justify="space-between"
             minH="100vh"
         >
+            <Alert
+                primaryText="This product is in beta:"
+                secondaryText="The smart contracts are peer reviewed with formal audits coming soon, please use at your own risk."
+                open={open}
+                setOpen={setOpen}
+            />
             <Header/>
             <Box
                 p={5}
